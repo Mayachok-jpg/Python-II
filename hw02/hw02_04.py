1d75cc2cbe6048d20a8da1cb4b4d93c7163d2488
Mayachok-jpg <maya@dihalt.ru>
2021-06-26 17:38:00 +0500

    исправления

--- hw02/hw02_04.py
+++ hw02/hw02_04.py
@@ -1,6 +1,9 @@
 def sum_of_row(n):
-    return 1 if n == 1 else sum_of_row(n-1)*(-0.5)
-
+    res = 1
+    if n == 0:
+        return n
+    res += sum_of_row(n-1)*(-0.5)
+    return res
 
 number = int(input('Число? '))
 print(sum_of_row(number))
