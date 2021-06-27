8bafcfc4bf24619fc819d0eb2778226c63b57a0c
Mayachok-jpg <maya@dihalt.ru>
2021-06-27 14:46:47 +0500

    исправления

--- hw03/hw03_01.py
+++ hw03/hw03_01.py
@@ -1,63 +1,103 @@
-from time import perf_counter_ns
+from time import perf_counter_ns, time
 
 dict_obj = {}
 lst_obj = []
 
+
 def timing(func):
     def wrapped(*args):
-        begin_time = perf_counter_ns()
+        begin_time = time()
         res = func(*args)
-        func_time = perf_counter_ns() - begin_time
-        return res, func_time
+        func_time = time() - begin_time
+        print(f'Время выполнения функции {func.__name__}: {func_time}')
+        return res
     return wrapped
 
+
 @timing
-def fill_dict(n):                           # O(N)
+def fill_dict_compl(n):                           # O(N)
     return {a: a for a in range (n)}        # добавление элемента O(1), перебор O(N).
 
+
 @timing
-def fill_lst(n):                            # O(N)
+def fill_lst_compl(n):                            # O(N)
     return [a for a in range (n)]           # добавление элемента O(1), перебор O(N).
 
+
+@timing
+def fill_list_append(lst_obj, n):
+    for i in range(n):
+        lst_obj.append(i)  # Вставка в конец списка, O(1)
+
+
+@timing
+def fill_list_insert(lst_obj, n):
+    for i in range(n):
+        lst_obj.insert(0, i)  # Вставка в начало списка, O(N)
+
+
+@timing
+def fill_dict(dict_obj, n):
+    for i in range(n):
+        dict_obj[i] = i      # O(1)
+
+@timing
+def change_list(lst_obj, n):
+    for i in range (n):
+        lst_obj[i] = lst_obj[i * 2]    # O(N)
+
+@timing
+def change_dict(dict_obj, n):
+    for i in range (n):
+        dict_obj[i] = i    # O(1)
+
+
+@timing
+def del_from_list(lst_obj, n):
+    for i in range (n):
+        lst_obj.pop(i)    # O(N)
+
+
+@timing
+def del_from_dict(dict_obj, n):
+    for i in range (n):
+        dict_obj.pop(i)    # O(1)
+
+
 n = 100000
-lst_obj, lst_time = fill_lst(n)
-dict_obj, dict_time = fill_dict(n)
+lst_obj = fill_lst_compl(n)
+dict_obj = fill_dict_compl(n)
+
+fill_list_append(lst_obj, n)
+fill_list_insert(lst_obj, n)
+
+fill_dict(dict_obj, n)
+
+change_list(lst_obj, n)
+change_dict(dict_obj, n)
+
+del_from_list(lst_obj, n)
+del_from_dict(dict_obj, n)
 
 
-print(f'Время сознания словаря {dict_time}')
-print(f'Время сознания списка {lst_time}')
 
 
-time = perf_counter_ns()
-len(dict_obj)                                              #O(1)
-print(f'Длина словаря: {perf_counter_ns() - time}')
 
-time = perf_counter_ns()
-len(lst_obj)                                               #O(1)
-print(f'Длина списка: {perf_counter_ns() - time}')
+'''
+Время выполнения функции fill_lst_compl: 0.008997201919555664
+Время выполнения функции fill_dict_compl: 0.021010160446166992
 
-time = perf_counter_ns()
-for el in dict_obj: pass                                   #O(N)
-print(f'Перебор словаря: {perf_counter_ns() - time}')
+Время выполнения функции fill_list_append: 0.015990257263183594
+Время выполнения функции fill_list_insert: 81.7020092010498
+Время выполнения функции fill_dict: 0.017991304397583008
 
-time = perf_counter_ns()
-for el in lst_obj: pass
-print(f'Перебр списка: {perf_counter_ns() - time}')         #O(N)
+Время заполнение словаря сопоставимо со временем заполнения списка при использовании append, insert же намного медленее 
 
-time = perf_counter_ns()
-el = dict_obj[5]                                            #O(1)
-print(f'Получение элемента словаря: {perf_counter_ns() - time}')
+Время выполнения функции change_list: 0.018999099731445312
+Время выполнения функции change_dict: 0.04400300979614258
 
-time = perf_counter_ns()
-el = lst_obj[5]                                             #O(1)
-print(f'Получение элемента списка: {perf_counter_ns() - time}')
+Время выполнения функции del_from_list: 61.113001585006714
+Время выполнения функции del_from_dict: 0.023004531860351562
 
-time = perf_counter_ns()
-if el in dict_obj.values(): pass                            #O(N)
-print(f'Проверка наличия элемента: {perf_counter_ns() - time}')
-
-time = perf_counter_ns()
-if el in lst_obj: pass                                      #O(N)
-print(f'Проверка наличия элемента: {perf_counter_ns() - time}')
-
-'''По времени все операции со словарем оказались более затратными. Видимо, из-за хеша'''
+Удаление из словаря намного быстрее, константная сложность против линейной.
+'''
--- hw03/hw03_04.py
+++ hw03/hw03_04.py
@@ -3,7 +3,11 @@ from uuid import uuid4
 
 
 def check_cache(url_obj):
-    return cache_dict.setdefault(url_obj, sha256(salt.encode() + url_obj.encode()))
+    if cache_dict.get(url_obj) is None:
+        cache_dict[url_obj] = sha256(salt.encode() + url_obj.encode())
+        print(f'Значение добавлено, {url_obj}, {cache_dict[url_obj]}')
+    else:
+        print(f'Значение есть в кеше:, {url_obj}, {cache_dict[url_obj]}')
 
 
 cache_dict = {}
@@ -12,12 +16,22 @@ salt = uuid4().hex   # статическая соль
 check_cache('ya.ru')
 check_cache('vk.com')
 check_cache('ya.ru')
-check_cache('https://pythonworld.ru/')
 
-print(check_cache('sur'))
+
+url = 'ya.ru'
+print(cache_dict.setdefault(url, sha256(salt.encode() + url.encode())))
+url = 'sur'
+print(cache_dict.setdefault(url, sha256(salt.encode() + url.encode())))
+
+print(cache_dict)
+
+for el in cache_dict.values():
+    print(el.hexdigest())
 
 for el in cache_dict.keys():
     print(el, cache_dict[el].hexdigest())
+
+check_cache('ya.ru')
 
 
 
